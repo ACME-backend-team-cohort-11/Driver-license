5bb2472292708ffd63395ed291d533bbf1c94254
emekeo <gistwithcollins@gmail.com>
2024-07-06 06:50:11 -0400

    License Validation Views

--- licenseValidation/views.py
+++ licenseValidation/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from .models import DriverLicense
+from django.http import HttpResponse
 
-# Create your views here.
+def verify_license(request):
+    if request.method == 'GET':
+        license_number = request.GET.get('license_number')
+        try:
+            license = DriverLicense.objects.get(license_number=license_number)
+            context = {'license': license}
+        except DriverLicense.DoesNotExist:
+            context = {'error': 'License not found'}
+        return render(request, 'licenses/verify_license.html', context)
+
+def license_status(request):
+    if request.method == 'GET':
+        license_number = request.GET.get('license_number')
+        try:
+            license = DriverLicense.objects.get(license_number=license_number)
+            status = license.status
+            return HttpResponse(f"License status: {status}")
+        except DriverLicense.DoesNotExist:
+            return HttpResponse("License not found")
