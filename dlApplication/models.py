b17fe79350f1e870e5c22915dfbcd4eaafe0dad6
Richie Mugambi <richiemugambi4@gmail.com>
2024-07-08 14:14:22 +0300

    feat: removed all migrations and moved custom user to accounts

--- dlApplication/models.py
+++ dlApplication/models.py
@@ -1,16 +1,6 @@
-from django.contrib.auth.models import Group, Permission, AbstractUser
 from django.db import models
+from accounts.models import CustomUser
 import uuid
-
-class CustomUser(AbstractUser):
-    username = models.CharField(max_length=50, unique=True)
-    nin = models.CharField(max_length=20, unique=True)
-    phone_number = models.CharField(max_length=20, unique=True)
-    user_permissions = models.ManyToManyField(Permission, related_name='custom_user_permissions', blank=True)
-    groups = models.ManyToManyField(Group, related_name='custom_user_groups', blank=True)
-
-    def __str__(self):
-        return self.username
 
 class NewApplication(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
